fcee7f02b16b4187de39912484fcccf18431a533
Alifahes <31044906+Alifahes@users.noreply.github.com>
2021-01-20 03:54:30 +0200

    Add files via upload

---
+++ o.js
@@ -0,0 +1,12 @@
+var a= document.getElementById("aa");
+
+a.onmousemove=()=>{
+    'use strict';
+    var c=document.getElementById("n");
+    c.style.display="block";
+}
+a.onmouseout=()=>{
+    'use strict';
+    var c=document.getElementById("n");
+    c.style.display="none";
+}
